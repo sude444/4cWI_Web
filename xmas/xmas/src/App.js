df47e4f74d56b1cb7963c77e9a036702e0c51a2b
sude <sude.bayar@student.htldornbirn.at>
2023-12-19 09:11:02 +0100

    xmas_website_fertig

---
+++ xmas/xmas/src/App.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        The BEST christmas cookie recipe :D
+      </header>
+      <body>
+        <h1>Wollt ihr auch zuckersüße Wheinachtskekse backen?</h1>
+        <img class="keksBild1" src="bilder/keks.jpg"/>
+        <h2>Rezept:</h2>
+        <p class="UB">Zutaten:</p>
+        <p>Für die Kekse</p>
+        <p>220g Butter, zimmerwarm</p>
+        <p>110g Puderzucker</p>
+        <p>1 Ei (S oder M)</p>
+        <p>2,5TL Vanille Extrakt</p>
+        <p>Mark einer Vanilleschote</p>
+        <p>Schale einer Bio Zitrone</p>
+        <p>280g Mehl - falls der Teig trotz ausreichender Kühlung zu klebrig ist, noch 10-20g dazu zu geben </p>
+        <p>Prise Salz</p>
+        <p class="UB">Für die Verzierung / Royal Icing:</p>
+        <p>220g Puderzucker</p>
+        <p>1 EL Meringue Powder (ich habe meins in den USA gekauft, man kann es aber auch hier online bestellen)</p>
+        <p>2-3 EL Wasser</p>
+        <p>Lebensmittelfarben nach Wahl (am besten Gelfarben, die man auch hier bestellen kann)</p>
+        <p class="UB">Zubereitung</p>
+        <p class="erklärung"> Mehl mit Salz vermengen und beiseite stellen. Butter mit Puderzucker schaumig schlagen (am Besten mit der Küchenmaschine, wir nehmen diese hier),   dann Ei,   Vanilla Extract,   
+          Vanillemark und die fein geriebene Zitronenschale unterrühren.   Jetzt Mehl unterrühren, nur so lange, bis alles vermengt ist. Den Teig zu zwei Kugeln formen, 
+          dabei die Hände falls nötig leicht mit Wasser anfeuchten, flach drücken (das erleichtert das ausrollen nachher) und für mindestens 1-2 Stunden kühl stellen - über Nacht ist auch super.
+          Wenn der Teig fest genug ist zum Ausrollen auf einer gut bemehlten Arbeitsfläche auswellen, nicht zu dünn - wir wollen schöne dicke Plätzchen, keine dünnen, brüchigen. 
+          Den ersten Teig zügig noch kühl verarbeiten bzw. nach Belieben mit Förmchen ausstechen, während der zweite weiterhin im Kühlschrank wartet, bis er ausgerollt werden kann.
+          Im vorgeheizten Ofen bei 190°C für 8-10 Minuten (je nach Dicke) backen und vor dem Dekorieren vollständig auskühlen lassen.
+          Für die Verzierung: Alle Zutaten mit der Küchenmaschine oder der KitchenAid vermengen und 7-8 Minuten mittelschnell rühren lassen, bis das Icing einen matt-glänzenden Ton hat, 
+          dann das Icing luftdicht verschließen (sonst beginnt es recht schnell auszuhärten). Es kann jetzt auf kleine Döschen aufgeteilt, mit den verschiedenen Lebensmittelfarbe eingefärbt 
+          und dann per Spritzbeutel auf den Cookies verteilt werden, dabei das Icing zwischendurch bei Nicht-Gebrauch bitte wieder luftdicht verschließen. Falls es zum Spritzen noch etwas zu fest sein sollte, 
+          kann es mit wenig Wasser etwas verdünnt werden, bis die gewünschte Konsistenz erreicht ist.</p>
+        
+        <img class="keks2" src="bilder/keks2.jpg"/>
+
+      </body>
+
+
+    </div>
+  );
+}
+
+export default App;
